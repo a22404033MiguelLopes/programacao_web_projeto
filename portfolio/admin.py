79d35c40ae9db6595fa12cb0bcf518d2dd7f5199
miguellopes <a22404033@alunos.ulht.pt>
2026-04-11 22:05:39 +0000

    URL config and adding Licenciature with Admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Licenciatura
 
 # Register your models here.
+@admin.register(Licenciatura)
+class LicenciaturaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'instituicao', 'ects')
+    search_fields = ('nome', 'instituicao')
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=100)
+    instituicao = models.CharField(max_length=100)
+    ects = models.IntegerField()
+    website = models.URLField(blank=True, null=True)
+    logo = models.ImageField(upload_to='licenciatura/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
